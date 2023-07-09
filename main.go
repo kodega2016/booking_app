6e205e9318960288219df97203f436e6f909944e
kodega2016 <khadgalovecoding2016@gmail.com>
2023-07-10 04:47:29 +0545

    added templates layouts

--- main.go
+++ main.go
@@ -9,16 +9,6 @@ import (
 // port for the application
 const port = ":8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is home page")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is about page")
-}
-
 // main is the main entry point of the application
 func main() {
 	http.HandleFunc("/", Home)
