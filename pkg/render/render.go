26600b84a8babf6434573c2fe734cf65d3ad25f9
kodega2016 <khadgalovecoding2016@gmail.com>
2023-07-10 07:09:39 +0545

    added template cache for caching the parsed templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,17 +1,58 @@
 package render
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	parsedTemplate.Execute(w, nil)
+}
+
+// tc holds the template cache
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if the template is already present in the cache,
+	_, isOk := tc[t]
+
+	if isOk {
+		fmt.Println("template is already present in the cache,using from the cache")
+	} else {
+		// need to create template cache
+		createTemplateCache(t)
+	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+	return err
 }
