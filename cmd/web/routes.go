b50f83c06b9f0e90ab552d0925710e91bb387874
kodega2016 <khadgalovecoding2016@gmail.com>
2023-07-13 07:30:35 +0545

    added file server added asset images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,6 +24,10 @@ func routes(app *config.AppConfig) http.Handler {
 	// use load session load
 	mux.Use(SessionLoad)
 
+	// file server
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", handlers.Repo.Home)
 	mux.HandleFunc("/about", handlers.Repo.About)
 	return mux
